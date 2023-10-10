a2a282b14e05e24b45cad56065f6921925604d71
Niroj Neupane Khatri <knirojneupane@gmail.com>
2023-10-10 21:41:11 +0545

    Complete project

--- script.js
+++ script.js
@@ -5,70 +5,114 @@ function Book(title, author, pages, status) {
     this.pages = pages
     this.status = status
 }
-myLibrary[0] = new Book('The Hobbit', 'J.J.Tolkein', 296, 'Completed');
-myLibrary[1] = new Book('The Hobbi', 'J.J.Tolkein', 296, 'Reading');
-//now to add new books
-function addNewBook(book) {
-    myLibrary.push(book);
-}
-//render books
-
 
 const booksContainer = document.getElementById('booksContainer');
 const formContainer = document.getElementById('formContainer');
-for (let i = 0; i < myLibrary.length; i++) {
 
-    const booksdiv = document.createElement('div');
-    const bookstitle = document.createElement('div');
-    const booksauthor = document.createElement('div');
-    const bookspages = document.createElement('div');
-    const booksstatus = document.createElement('button');
-    const booksdrop = document.createElement('button');
+myLibrary[0] = new Book('The Hobbit', 'JJ Tolkein', 222, 'Completed');
+//function to render DOM 
+function displayBooks() {
+    booksContainer.innerHTML = '';
+    for (let i = 0; i < myLibrary.length; i++) {
+        const booksdiv = document.createElement('div');
+        const bookstitle = document.createElement('div');
+        const booksauthor = document.createElement('div');
+        const bookspages = document.createElement('div');
+        const booksstatus = document.createElement('button');
+        const booksdrop = document.createElement('button');
 
-    booksdiv.setAttribute('class', 'books');
-    bookstitle.setAttribute('class', 'bookstitle');
-    booksauthor.setAttribute('class', 'booksauthor');
-    bookspages.setAttribute('class', 'bookspages');
-    booksstatus.setAttribute('class', 'booksstatus');
-    if (myLibrary[i].status === 'Completed')
-        booksstatus.classList.toggle('completed');
-    booksdrop.setAttribute('class', 'booksdrop');
+        booksdiv.setAttribute('class', 'books');
+        bookstitle.setAttribute('class', 'bookstitle');
+        booksauthor.setAttribute('class', 'booksauthor');
+        bookspages.setAttribute('class', 'bookspages');
+        booksstatus.setAttribute('class', 'booksstatus');
+        booksstatus.setAttribute('index', i);
+        booksdrop.setAttribute('class', 'booksdrop');
+        booksdrop.setAttribute('index', i);
 
-    bookstitle.textContent = myLibrary[i].title;
-    booksauthor.textContent = myLibrary[i].author;
-    bookspages.textContent = myLibrary[i].pages + ' pages';
-    booksstatus.textContent = myLibrary[i].status;
-    booksdrop.textContent = 'Drop';
+        bookstitle.textContent = myLibrary[i].title;
+        booksauthor.textContent = myLibrary[i].author;
+        bookspages.textContent = myLibrary[i].pages + ' pages';
+        if (myLibrary[i].status === 'Completed')
+            booksstatus.classList.add('completed');
+        else
+            booksstatus.classList.remove('completed');
+        booksstatus.textContent = myLibrary[i].status;
+        booksdrop.textContent = 'Drop';
+        booksdiv.appendChild(booksdrop);
+        booksdiv.appendChild(bookstitle);
+        booksdiv.appendChild(booksauthor);
+        booksdiv.appendChild(bookspages);
+        booksdiv.appendChild(booksstatus);
+        booksdiv.appendChild(booksdrop);
+        booksContainer.appendChild(booksdiv);
 
-    booksdiv.appendChild(booksdrop);
-    booksdiv.appendChild(bookstitle);
-    booksdiv.appendChild(booksauthor);
-    booksdiv.appendChild(bookspages);
-    booksdiv.appendChild(booksstatus);
-    booksdiv.appendChild(booksdrop);
-    booksContainer.appendChild(booksdiv);
+        //event listener to drop(delete) a book
+        booksdrop.addEventListener('click', () => {
+            let index = booksdrop.getAttribute('index');
+            myLibrary.splice(index, 1);
+            displayBooks();
+        })
+        //event listener for changing book status
+        booksstatus.addEventListener('click', () => {
+            let index = booksstatus.getAttribute('index');
+            if (myLibrary[index].status === 'Reading')
+                myLibrary[index].status = 'Completed';
+            else
+                myLibrary[index].status = 'Reading';
+            displayBooks();
+        })
+    }
+
 }
-const bookStatusBtn = document.querySelectorAll('.booksstatus');
-bookStatusBtn.forEach((statusbtn) => {
-    statusbtn.addEventListener('click', () => {
-        statusbtn.classList.toggle("completed");
-        if (statusbtn.textContent === 'Completed')
-            statusbtn.textContent = 'Reading';
-        else if (statusbtn.textContent === 'Reading')
-            statusbtn.textContent = 'Completed'
-    })
-});
+
+//function to validate form
+function validate(title, author, pages) {
+    if (title === '' || author === '' || pages === '') {
+        alert('Must fill all sections');
+        return false;
+    }
+    return true;
+}
+
+
+//event listener for adding new books
 const displaySwitchButtons = document.querySelectorAll('.displaySwitch');
 displaySwitchButtons.forEach((btns) => {
-    btns.addEventListener('click',()=>{
-        if(btns.getAttribute('id') == 'addNew'){
-            formContainer.setAttribute('style','display:flex');
-            booksContainer.setAttribute('style','display:none');
+    btns.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (btns.getAttribute('id') == 'addNew') {
+            formContainer.setAttribute('style', 'display:flex');
+            booksContainer.setAttribute('style', 'display:none');
         }
-        else if(btns.getAttribute('id') == 'close' ||
-                btns.getAttribute('id') == 'add'){
-            formContainer.setAttribute('style','display:none');
-            booksContainer.setAttribute('style','display:flex');
+        else if (btns.getAttribute('id') == 'close') {
+            formContainer.setAttribute('style', 'display:none');
+            booksContainer.setAttribute('style', 'display:flex');
+        }
+        else if (btns.getAttribute('id') == 'add') {
+            const title = document.getElementById('booktitle').value;
+            const author = document.getElementById('bookauthor').value;
+            const pages = document.getElementById('bookpages').value;
+            const status = document.getElementById('bookstatus').value;
+            if (validate(title, author, pages) == true) {
+                const book = new Book(title, author, pages, status);
+                myLibrary.push(book);
+                formContainer.setAttribute('style', 'display:none');
+                booksContainer.setAttribute('style', 'display:flex');
+                document.getElementById('form').reset();
+                displayBooks();
+            }
         }
     })
 });
+
+//event listener for resetting form
+const resetbtn = document.querySelector('#resetbtn');
+resetbtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('form').reset();
+    displayBooks();
+})
+
+
+document.addEventListener('DOMContentLoaded', displayBooks);
